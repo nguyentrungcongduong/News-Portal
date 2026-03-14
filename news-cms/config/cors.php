2485c7b34658707269a567856061da870ddc4e4b
Nguyen Trung Cong Duong <nguyentrungcongduong@gmail.com>
2026-03-14 20:48:43 +0700

    fix: make CORS origins dynamic and enable credentials for Sanctum

--- news-cms/config/cors.php
+++ news-cms/config/cors.php
@@ -19,14 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:5174',
-    ],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173')),
 
     'allowed_origins_patterns' => [],
 
@@ -36,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
